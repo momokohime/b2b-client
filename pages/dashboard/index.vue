d1ab909c05ef4b3f5e6bc5265f426fb68fd5763b
momokohime <momoko-hime@hotmail.com>
2019-10-09 13:59:27 -0600

    tercero

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -51,8 +51,8 @@
           <b-button-toolbar  class="float-right" aria-label="Toolbar with buttons group">
             <b-form-radio-group @input="fetchDashboard()"  class="mr-3" id="radiosBtn" buttons button-variant="outline-secondary" v-model="selected" name="radiosBtn">
               <b-form-radio class="mx-0" value="JMDIOFWENFO" >{{ $t('day') }}</b-form-radio>
-               <b-form-radio class="mx-0" value="Weekly" >{{ $t('week') }}</b-form-radio>
-              <b-form-radio class="mx-0" value="Month">{{ $t('month') }}</b-form-radio>
+               <b-form-radio class="mx-0" value="efwe" >{{ $t('week') }}</b-form-radio>
+              <b-form-radio class="mx-0" value="fwef">{{ $t('month') }}</b-form-radio>
               <b-form-radio class="mx-0" value="Year" >{{ $t('year') }}</b-form-radio>
             </b-form-radio-group>
           </b-button-toolbar>
